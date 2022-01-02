5a5a9c08e2873b184b453cc4d061d9190741f7b0
gilmadson <gilmadsonmacedo@gmail.com>
2022-01-02 01:05:30 -0300

    primeiro commit configurado servidor e sql

---
+++ src/server.js
@@ -0,0 +1,16 @@
+'use strict';
+const express = require('express')
+const cors = require('cors')
+const bodyparser = require('body-parser')
+
+
+const config = require('./config/config.js')
+
+
+const app = express()
+
+app.use(cors())
+app.use(bodyparser.json())
+
+app.listen(config.port, () => console.log('Server is listening on http://localohost:' + config.port));
+
